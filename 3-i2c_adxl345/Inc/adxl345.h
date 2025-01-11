0621cc945fcd00098afda144ab1c2885b0a91e85
Akhil-Titus <akhiltitusvalooran@gmail.com>
2025-01-11 16:59:23 +0530

    fix: needs debugging

--- 3-i2c_adxl345/Inc/adxl345.h
+++ 3-i2c_adxl345/Inc/adxl345.h
@@ -13,7 +13,24 @@
 
 #include "i2c.h"
 
+#define DEVID_R 0X00
+#define DEVICE_ADDR 0X53
+#define DATA_FORMAT_R 0x31
+#define POWER_CTL_R 0x2D
+#define DATA_START_ADDR 0x32
+#define DATA_FORMAT_R 0x31
+
+#define FOUR_G 0x01
+#define RESET 0x00
+#define SET_MEASURE_B 0x08
+
+
+
 void adxl345_module_test_function_led(void);
+
+void adxl_init(void);   
+
+void adxl_read_values(uint8_t reg);
 
 #endif
 
