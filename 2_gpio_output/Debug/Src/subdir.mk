6a7619ef5ca53a16d392b0bbbee193110879f8ca
Akhil-Titus <akhiltitusvalooran@gmail.com>
2025-01-11 15:25:42 +0530

    fix : env change

--- 2_gpio_output/Debug/Src/subdir.mk
+++ 2_gpio_output/Debug/Src/subdir.mk
@@ -22,7 +22,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -c -I../Inc -I"/home/akhiltitus/Documents/Embedded systems/stm32_code/chip_headers/CMSIS/Device/ST/STM32F1xx/Include" -I"/home/akhiltitus/Documents/Embedded systems/stm32_code/chip_headers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F1 -DSTM32F103C8Tx -c -I../Inc -I"C:/Users/Akhil/Documents/personal-projects/bare-metal-stm32/chip_headers/CMSIS/Include" -I"C:/Users/Akhil/Documents/personal-projects/bare-metal-stm32/chip_headers/CMSIS/Device/ST/STM32F1xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Src
 
